054e58955e0a7db978ff96bacad7aa78ab81d5c4
Shijina S <129263578+Shijina843@users.noreply.github.com>
2023-12-03 00:41:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const data = {
+	"quotes": [
+		{
+			"web":"Web",
+			"dev":"Developer"
+			 
+		},
+		{
+			"web":"UI/UX",
+			"dev":"Designer"
+			
+		},
+		{
+			"web":"Tech",
+			"dev":"Ethusiast"
+			
+		}
+    ]
+};
+
+  
+const qoutes = data.quotes;
+
+function getRandomQuote() {
+	arraysize = qoutes.length;
+	random = Math.random() * arraysize;
+	randomnumber = Math.floor(random);
+	return qoutes[randomnumber];
+
+}
+
+
+
+function printOutput() {
+	var Quote = getRandomQuote();
+	let web = document.getElementById("web");
+	let dev = document.getElementById("dev");
+	web.innerHTML = Quote.web;
+	dev.innerHTML = Quote.dev;
+
+}
+
+window.setInterval(printOutput, 1000);
+
+
